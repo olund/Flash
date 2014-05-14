d24105ee141443a28e809dfcfc5301dcb141697c
olund <henke.olund@gmail.com>
2014-05-14 16:18:15 +0200

    Updated cflashtest

--- test/Flash/CFlashTest.php
+++ test/Flash/CFlashTest.php
@@ -4,16 +4,14 @@ namespace olund\Flash;
 
 class CFlashTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
      * Test if the session is null after a clear.
      * @return
      */
     public function testClear()
     {
-        $this->flash = new \olund\Flash\CFlash();
-
-        $this->flash->clear();
+        $flash = new \olund\Flash\CFlash();
+        $flash->clear();
         $this->assertEquals(null, $_SESSION['flash']);
     }
 }
