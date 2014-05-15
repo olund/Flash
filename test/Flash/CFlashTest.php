4436e5cacc249790800412ae8983cf4ac392d4bf
olund <henke.olund@gmail.com>
2014-05-15 09:11:52 +0200

    Updated test cases

--- test/Flash/CFlashTest.php
+++ test/Flash/CFlashTest.php
@@ -4,37 +4,40 @@ namespace olund\Flash;
 
 class CFlashTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function setup()
+    {
+        $this->flash = new \olund\Flash\CFlash();
+    }
+
     /**
      * Test if the session is array after a clear WITH restart.
      * @return
      */
-    public function testClearWithRestart()
-    {
-        $this->flash = new \olund\Flash\CFlash();
-        $this->flash->clear(true);
-        $this->assertEquals([], $_SESSION['flash']);
-    }
-
-    /**
-     * Test if the session is null after a clear and the session is not restarted.
-     * @return
-     */
     public function testClear()
     {
-        $this->flash = new \olund\Flash\CFlash();
-        $this->flash->clear(false);
-        $this->assertEquals(null, $_SESSION['flash']);
-    }
-
-    /*
-    public function testSuccess()
-    {
-
+        $this->flash->clear(true);
+        $exp = [];
+        $res = $_SESSION['flash'];
+        $this->assertEquals($res, $exp, "Session is not array..");
     }
 
     public function testGet()
     {
-       // $exp = "";
+        $this->flash->error('test');
+        $this->flash->notice('test');
+        $this->flash->warning('test');
+        $this->flash->success('test');
 
-    }*/
+        $res = $this->flash->get();
+        $exp = "<div class=errorMessage><i class='fa fa-times-circle'></i> test</div><div class=noticeMessage><i class='fa fa-info'></i> test</div><div class=warningMessage><i class='fa fa-warning'></i> test</div><div class=successMessage><i class='fa fa-check'></i> test</div>";
+        $this->assertEquals($res, $exp, "flash->get() mismatch");
+    }
+
+    public function testGetEmpty()
+    {
+        $res = $this->flash->get();
+        $exp = null;
+        $this->assertEquals($res, $exp, "flash->get() is not null..");
+    }
 }
