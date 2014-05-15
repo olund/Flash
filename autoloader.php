4465d7040fb51eef31f908ccffd0dbc1c0d5b41c
olund <henke.olund@gmail.com>
2014-05-15 08:35:50 +0200

    updated autoloader with my prefix and the FlashTest loads now

--- autoloader.php
+++ autoloader.php
@@ -15,7 +15,7 @@ spl_autoload_register(function ($class) {
 
     // project-specific namespace prefix
     //$prefix = 'Foo\\Bar\\';
-    $prefix = 'Jox\\';
+    $prefix = 'olund\\';
 
     // base directory for the namespace prefix
     $base_dir = __DIR__ . '/src/';
--- test/Flash/CFlashTest.php
+++ test/Flash/CFlashTest.php
@@ -5,13 +5,36 @@ namespace olund\Flash;
 class CFlashTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Test if the session is null after a clear.
+     * Test if the session is array after a clear WITH restart.
+     * @return
+     */
+    public function testClearWithRestart()
+    {
+        $this->flash = new \olund\Flash\CFlash();
+        $this->flash->clear(true);
+        $this->assertEquals([], $_SESSION['flash']);
+    }
+
+    /**
+     * Test if the session is null after a clear and the session is not restarted.
      * @return
      */
     public function testClear()
     {
-        $flash = new \olund\Flash\CFlash();
-        $flash->clear();
+        $this->flash = new \olund\Flash\CFlash();
+        $this->flash->clear(false);
         $this->assertEquals(null, $_SESSION['flash']);
     }
+
+    /*
+    public function testSuccess()
+    {
+
+    }
+
+    public function testGet()
+    {
+       // $exp = "";
+
+    }*/
 }
